10624c556b6e27f104624419f262e704f76ffeb9
Stefan Schmidt <sschmidt@pivotal.io>
2023-09-28 10:10:37 +0200

    initial push

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -l
+
+sdk use java 11.0.18-amzn
+
+cd hello-vienna && javac HelloVienna.java
---
+++ hello-vienna/HelloVienna.java
@@ -0,0 +1,7 @@
+// Plain Old Java
+
+class HelloVienna {
+    public static void main(String[] args) {
+        System.out.println("Java welcomes you to the CNCF Meetup in Vienna"); 
+    }
+}
